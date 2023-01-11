2d65481a80f4c3e4d4f9c97fcc86cd9eb4f2391d
Guillaume Gosselin <guillaume@bauhem.com>
2023-01-11 10:47:08 -0500

    up

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,25 +1,7 @@
 <template>
-  <nav class="navbar">
-    <div class="container">
-      <div class="navbar-brand">
-        <nuxt-link class="navbar-item" to="/">
-          <img src="/bulma.png" alt="Logo" />
-        </nuxt-link>
-        <span class="navbar-burger burger" v-on:click="toggleNavbar">
-          <span></span>ß
-          <span></span>
-          <span></span>
-        </span>
-      </div>
-      <div :class="['navbar-menu', navbarOpen && 'is-active']">
-        <div class="navbar-end">
-          <nuxt-link class="navbar-item is-active is-size-5 has-text-weight-semibold" to="/">
-            Home
-          </nuxt-link>
-        </div>
-      </div>
-    </div>
-  </nav>
+  <div>
+    Navbar
+  </div>
 </template>
 
 <script setup lang="ts">
