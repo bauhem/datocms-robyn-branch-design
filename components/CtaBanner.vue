c8aa8515fde60f9e7823f1d81bb54a87ccdd6aa5
Guillaume Gosselin <guillaume@bauhem.com>
2023-01-30 11:55:55 -0500

    up

--- components/CtaBanner.vue
+++ components/CtaBanner.vue
@@ -1,8 +1,12 @@
 <template>
-  <section class="section bg-image wf-section"  
-v-bind:style="'min-height: 500px; padding-top: 125px; background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(' + item.image.responsiveImage.src + '); background-position: 0px 0px, 50% 50%; background-size: auto, cover; background-attachment: scroll, fixed;'"
-
->
+  <section
+    class="section bg-image wf-section"
+    v-bind:style="
+      'min-height: 500px; padding-top: 125px; background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(' +
+      item.image.responsiveImage.src +
+      '); background-position: 0px 0px, 50% 50%; background-size: auto, cover; background-attachment: scroll, fixed;'
+    "
+  >
     <div class="container">
       <div
         data-w-id="dc48abc7-64d1-0c91-2081-17775079252c"
