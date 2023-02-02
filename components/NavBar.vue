31ae140742ffd8877572cd77b2742efa1bc68a83
Guillaume Gosselin <guillaume@bauhem.com>
2023-02-02 14:43:46 -0500

    up

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,22 +2,22 @@
   <div data-animation="over-left" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar w-nav">
     <div class="nav-container w-container">
       <div class="nav-menu-wrapper">
-        <a href="/" class="brand w-nav-brand"><img src="/images/logo.webp"  height="25" alt=""></a>
+        <NuxtLink to="/" class="brand w-nav-brand"><img src="/images/logo.webp"  height="25" alt=""></NuxtLink>
         <nav role="navigation" class="nav-menu w-nav-menu">
           <div class="menu-wrap">
             <div class="tablet-menu">
-              <a href="/" class="brand-tablet w-nav-brand"><img src="/images/favicon.png"  alt=""><img src="/images/logo-small.webp"  alt="" class="logo-text"></a>
+              <NuxtLink to="/" class="brand-tablet w-nav-brand"><img src="/images/favicon.png"  alt=""><img src="/images/logo-small.webp"  alt="" class="logo-text"></NuxtLink>
               <div class="close-menu-button w-nav-button"><img src="/images/close.png"  width="16" alt="" class="close-icon"></div>
             </div>
             <div class="nav-block-left">
-              <a v-for="item in data.navLeft" :key="item.id" :href="item.parent != null ? '/' + item.parent.slug + '/' + item.slug : '/' + item.slug" class="nav-link">{{ item.title }}</a>
+              <NuxtLink v-for="item in data.navLeft" :key="item.id" :to="item.parent != null ? '/' + item.parent.slug + '/' + item.slug : '/' + item.slug" class="nav-link">{{ item.title }}</NuxtLink>
             </div>
-            <a href="/" aria-current="page" class="brand-menu w-nav-brand w--current">
+            <NuxtLink to="/" aria-current="page" class="brand-menu w-nav-brand w--current">
               <img src="/images/favicon.png"  alt="">
-              <img src="/images/logo-middle.webp"  alt="" class="logo-text"></a>
+              <img src="/images/logo-middle.webp"  alt="" class="logo-text"></NuxtLink>
             <div class="nav-block-right">
-             <a v-for="item in data.navRight" :key="item.id" :href="item.slug" class="nav-link">{{ item.title }}</a>
-             <a :href="data.contact.slug" class="button in-navbar">{{ data.contact.title }}</a>
+             <NuxtLink v-for="item in data.navRight" :key="item.id" :to="item.slug" class="nav-link">{{ item.title }}</NuxtLink>
+             <NuxtLink :to="data.contact.slug" class="button in-navbar">{{ data.contact.title }}</NuxtLink>
             </div>
           </div>
         </nav>
