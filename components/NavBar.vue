6acf36ec28b4df2d18393e7e20bd67badd46ce8b
Guillaume Gosselin <guillaume@bauhem.com>
2023-02-02 14:47:35 -0500

    up

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -29,6 +29,22 @@
   </div>
 </template>
 
+<script>
+export default {
+  mounted() {
+    this.$nextTick(function () {
+      $(document).ready(function () {
+        // eslint-disable-next-line no-undef
+        window.Webflow && window.Webflow.destroy();
+        window.Webflow && window.Webflow.ready();
+        window.Webflow && window.Webflow.require("ix2").init();
+        document.dispatchEvent(new Event("readystatechange"));
+      });
+    });
+  }
+};
+</script>
+
 <script setup >
 
 const { data } = await useGraphqlQuery({
