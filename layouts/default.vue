106a0baf5649c9df86435cd47520a69c3b549e70
Guillaume Gosselin <guillaume@bauhem.com>
2023-02-03 08:46:22 -0500

    up

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,27 @@
     <Footer v-bind:navLeft="data.navLeft" v-bind:navRight="data.navRight" v-bind:contact="data.contact"/>
 </template>
 
+<script>
+export default {
+  mounted() {
+    this.$nextTick(function () {
+      $(document).ready(function () {
+        if (window.innerWidth <= 768) {
+          $('.w-nav-menu').on('click', 'a', function() {
+            $('.w-nav-button').triggerHandler('tap');
+          });
+        }
+        // eslint-disable-next-line no-undef
+        window.Webflow && window.Webflow.destroy();
+        window.Webflow && window.Webflow.ready();
+        window.Webflow && window.Webflow.require("ix2").init();
+        document.dispatchEvent(new Event("readystatechange"));
+      });
+    });
+  }
+};
+</script>
+
 <script setup >
 
 const { data } = await useGraphqlQuery({
