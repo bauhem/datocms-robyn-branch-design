d008c0e0cc43aa1c60156437ec134d3966e6f978
Guillaume Gosselin <guillaume@bauhem.com>
2023-02-03 11:22:20 -0500

    up

--- components/Projects/HeroProject.vue
+++ components/Projects/HeroProject.vue
@@ -48,6 +48,17 @@ export default {
     item: Object,
     back: String,
   },
+  mounted() {
+    this.$nextTick(function () {
+      $(document).ready(function () {
+        // eslint-disable-next-line no-undef
+        window.Webflow && window.Webflow.destroy();
+        window.Webflow && window.Webflow.ready();
+        window.Webflow && window.Webflow.require("ix2").init();
+        document.dispatchEvent(new Event("readystatechange"));
+      });
+    });
+  }
 }
 </script>
 
