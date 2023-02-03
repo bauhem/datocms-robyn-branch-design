2a70460e19e616061664433da78b6090c8819f61
Guillaume Gosselin <guillaume@bauhem.com>
2023-02-03 11:01:24 -0500

    up

--- components/Hero.vue
+++ components/Hero.vue
@@ -87,15 +87,25 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   props: {
     item: Object
+  },
+   mounted() {
+    this.$nextTick(function () {
+      $(document).ready(function () {
+        // eslint-disable-next-line no-undef
+        window.Webflow && window.Webflow.destroy();
+        window.Webflow && window.Webflow.ready();
+        window.Webflow && window.Webflow.require("ix2").init();
+        document.dispatchEvent(new Event("readystatechange"));
+      });
+    });
   }
 };
 </script>
-
 <script setup lang="ts">
-import { Image } from 'vue-datocms'
+import { Image, StructuredText } from 'vue-datocms'
 
 </script>
