254449f0e5c1472d48ff85fbfafa892d88cf6eb1
Guillaume Gosselin <guillaume@bauhem.com>
2023-02-03 08:57:04 -0500

    up

--- components/HeroInterior.vue
+++ components/HeroInterior.vue
@@ -14,17 +14,6 @@
 export default {
   props: {
     item: Object
-  },
-   mounted() {
-    this.$nextTick(function () {
-      $(document).ready(function () {
-        // eslint-disable-next-line no-undef
-        window.Webflow && window.Webflow.destroy();
-        window.Webflow && window.Webflow.ready();
-        window.Webflow && window.Webflow.require("ix2").init();
-        document.dispatchEvent(new Event("readystatechange"));
-      });
-    });
   }
 };
 </script>
