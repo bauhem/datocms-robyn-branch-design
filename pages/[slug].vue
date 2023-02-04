9af167c0d5236591575342223a269ff94f8dc561
Guillaume Gosselin <guillaume@bauhem.com>
2023-02-04 10:08:50 -0500

    up

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -10,6 +10,20 @@
       v-bind:item="component.data"
       v-bind:slug="route.path"
     />
+    <section class="section white wf-section">
+    <div class="container">
+      <div class="projects-grid">
+        <div class="interior-wrapper">
+          <div class="rich-text w-richtext">
+               <datocms-structured-text :data="data.page.content" :renderBlock="renderBlock" />
+            </div>
+          <a href="contact.html" class="button in-navbar w-button">Contact Us</a>
+        </div>
+        <div class="interior-wrapper"><img src="/images/about-image.jpg" loading="lazy" sizes="(max-width: 479px) 94vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, 538px" srcset="/images/about-image-p-500.jpg 500w, /images/about-image.jpg 538w" alt="" class="image-robin"></div>
+      </div>
+    </div>
+  </section>
+
 
   </div>
 </template>
@@ -104,6 +118,20 @@ const components = data?.value?.page?.content?.blocks.map((item: { __typename: a
     return {};
   }
 })
+
+const renderBlock = ({ record }) => {
+  if (record.__typename) {
+    return null
+  }
+  return h(
+    'div',
+    {},
+    [
+      h('p', {}, "Don't know how to render a block!"),
+      h('pre', {}, JSON.stringify(record, null, 2)),
+    ]
+  );
+}
 
 useHead({
   htmlAttrs: {
